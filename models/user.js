a60bb8d2be893b1c49cd4e0547430836bd76a836
Gkeller25 <kellergaret@outlook.com>
2018-09-29 17:33:56 -0600

    added files for react passport

--- models/user.js
+++ models/user.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const bcrypt = require('bcryptjs');
+mongoose.promise = Promise
 
 const userSchema = new Schema({
   Name: { type: String, required: true },
@@ -8,13 +10,36 @@ const userSchema = new Schema({
     unique: true,
     match: [/.+@.+\..+/, "Please enter a valid e-mail address"]
   },
-  Password: String,
+  username: { type: String, unique: false, required: false },
+  password: { type: String, unique: false, required: false },
   Equipment:[String],
   Workout: [{ type: Schema.Types.ObjectId, ref: 'Workout' }],
   Exercise: [{ type: Schema.Types.ObjectId, ref: 'Exercise' }]
 
 });
 
-const User = mongoose.model("User", userSchema);
+// Define schema methods
+userSchema.methods = {
+	checkPassword: function (inputPassword) {
+		return bcrypt.compareSync(inputPassword, this.password)
+	},
+	hashPassword: plainTextPassword => {
+		return bcrypt.hashSync(plainTextPassword, 10)
+	}
+}
 
+// Define hooks for pre-saving
+userSchema.pre('save', function (next) {
+	if (!this.password) {
+		console.log('models/user.js =======NO PASSWORD PROVIDED=======')
+		next()
+	} else {
+		console.log('models/user.js hashPassword in pre save');
+		
+		this.password = this.hashPassword(this.password)
+		next()
+	}
+})
+
+const User = mongoose.model("User", userSchema);
 module.exports = User;
--- server.js
+++ server.js
@@ -2,16 +2,38 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const routes = require("./routes");
+const morgan = require('morgan')
+const session = require('express-session')
+const dbConnection = require('./models') 
+const MongoStore = require('connect-mongo')(session)
+const passport = require('./passport');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Define middleware here
+app.use(morgan('dev'))
 app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(bodyParser.json());
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+// Sessions
+app.use(
+	session({
+		secret: 'fraggle-rock', //pick a random string to make the hash that is generated secure
+		store: new MongoStore({ mongooseConnection: mongoose.connection }),
+		resave: false, //required
+		saveUninitialized: false //required
+	})
+)
+
+// Passport
+app.use(passport.initialize())
+app.use(passport.session()) // calls the deserializeUser
+
 // Add routes, both API and view
 app.use(routes);
 
